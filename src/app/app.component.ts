ededa7240dca7fde77779f79ffc92b8a44a22f12
Daniel <danielsantiagorodriguez72@gmail.com>
2024-09-14 10:25:32 +0200

    app configured to run ng serve (take into account the polling)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  template: 'probandods',
+  template: 'probandaaa',
   standalone: true,
-  //imports: [RouterOutlet],
-  //templateUrl: './app.component.html',
-  //styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  //title = 'FirstProjectAngular';
+  title = 'FirstProjectAngular';
 }
