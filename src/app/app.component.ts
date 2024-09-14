2c5abc8a50068f62bec241a60b859408e4acf7f6
Daniel <danielrodriguez@carnicassunyer.com>
2024-09-14 09:16:32 +0200

    configuring package json to inicialize only with ng serve in docker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  template: 'probandods',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  //imports: [RouterOutlet],
+  //templateUrl: './app.component.html',
+  //styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'FirstProjectAngular';
+  //title = 'FirstProjectAngular';
 }
